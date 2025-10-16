54fec1280635884e9a44cc9e906045e2b5a6c098
luciosugastti <luciosugasttipelizzari@gmail.com>
2025-10-16 00:57:18 -0300

    agregado de theme provider en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterApp } from './router/RouterApp.jsx'
-import './index.css'
-import { ChatProvider } from './context/ChatContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterApp } from "./router/RouterApp.jsx";
+import "./index.css";
+import { ChatProvider } from "./context/ChatContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ChatProvider>
-      <RouterApp />
-    </ChatProvider>
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <ChatProvider>
+        <RouterApp />
+      </ChatProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
